49fc294ed30f8246abbeef84b51d28371b4d54f0
Arnaldo Capo <arnaldo.capo@gmail.com>
2017-06-27 00:41:57 +0200

    First working version of the timer

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
+'use strict'
+
 const {app, Tray, net, Menu} = require('electron')
 const path = require('path')
 const fetch = require('electron-fetch')
 const Datastore = require('nedb')
+const moment = require('moment')
+const humanizeDuration = require('humanize-duration')
+const shortEnglishHumanizer = humanizeDuration.humanizer({
+  language: 'shortEn',
+  languages: {
+    shortEn: {
+      y: function() { return 'y' },
+      mo: function() { return 'mo' },
+      w: function() { return 'w' },
+      d: function() { return 'd' },
+      h: function() { return 'h' },
+      m: function() { return 'm' },
+      s: function() { return 's' },
+      ms: function() { return 'ms' },
+    }
+  }
+})
 var db = {}
+var clock = 0
+
 db.sessions = new Datastore({ filename: './db/sessions.db', autoload: true })
 db.sessions.loadDatabase()
 
@@ -16,8 +37,7 @@ app.dock.hide()
 
 app.on('ready', () => {
   createTray()
-  // startTick()
-  // fetchPrice()
+  checkClockStatus()
 })
 
 const createTray = () => {
@@ -32,12 +52,13 @@ const createTray = () => {
 
   // tray.setContextMenu(contextMenu)
   tray.on('click', (event) => {
-
     console.log('tray click')
 
-    db.sessions.find({}).sort({time: -1}).limit(1).exec((err, lastDoc) => {
-
-      console.log('lastDoc', lastDoc)
+    db.sessions
+      .find({})
+      .sort({time: -1})
+      .limit(1)
+      .exec((err, lastDoc) => {
 
       const isStart = (!lastDoc.length || lastDoc[0].start === false) ? true : false
       const doc = {
@@ -47,12 +68,61 @@ const createTray = () => {
 
       db.sessions.insert(doc, (err, newDoc) => {
         console.log('newDoc', newDoc)
+
+        if (newDoc.start)
+          return startClock(newDoc)
+
+        stopClock()
+          
       })
 
     })
   })
 }
 
-const startTick = () => {
+const checkClockStatus = () => {
+
+  db.sessions
+    .find({})
+    .sort({time: -1})
+    .limit(1)
+    .exec((err, lastDoc) => {
+
+      if (!lastDoc.length || lastDoc[0].start === false)
+        return
+      
+      startClock(lastDoc[0])
+  })
+}
+
+const startClock = (newDoc) => {
+  console.log('tick ')
+
+  const clockCore = () => {
+    console.log('t0ck', newDoc)
+
+    const startTime = moment(newDoc.time)
+    const now = moment()
+
+    const diff = now.diff(startTime, 'ms')
+    console.log('diff', diff)
+    const title = shortEnglishHumanizer(
+      diff,
+      {
+        round: true,
+        units: ['m']
+      }
+    )
+    tray.setTitle(title)
+    
+  }
   
+  clock = setInterval(clockCore, 60000)
+  clockCore()
+}
+
+const stopClock = () => {
+  clearInterval(clock)
+  clock = 0
+  tray.setTitle('')
 }
