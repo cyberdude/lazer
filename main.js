34ba0d47d1c404cb96e3321c1d832d67686e2de5
Arnaldo Capo <arnaldo.capo@gmail.com>
2017-07-04 00:18:28 +0200

    Changed model for project items. Adding a project name

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const fetch = require('electron-fetch')
 const Datastore = require('nedb')
 const moment = require('moment')
 const humanizeDuration = require('humanize-duration')
+const _ = require('lodash')
+
 const shortEnglishHumanizer = humanizeDuration.humanizer({
   language: 'shortEn',
   languages: {
@@ -23,7 +25,29 @@ const shortEnglishHumanizer = humanizeDuration.humanizer({
 })
 var db = {}
 var clock = 0
-
+var contextMenu = {}
+var menuItems = [
+  {
+    label: 'Start', type: 'normal' , click: () => {
+      tickClick()
+    }
+  },
+  {
+    label: 'Tezos', type: 'radio' , click: () => {
+      
+    }
+  },
+  {
+    label: 'Winevento', type: 'radio' , click: () => {
+      
+    }
+  },
+  {
+    label: 'Quit', type: 'normal', click: () => {
+      app.quit()
+    }
+  }
+]
 db.sessions = new Datastore({ filename: './db/sessions.db', autoload: true })
 db.sessions.loadDatabase()
 
@@ -40,43 +64,102 @@ app.on('ready', () => {
   checkClockStatus()
 })
 
+const listLogs = () => {
+  db.sessions
+      .find({
+        start : {
+          $gt: (new Date('2017-07-01T01:00:00.279Z'))
+        }
+      })
+      .sort({start: -1 })
+      .exec((err, docs) => {
+        console.log(docs)
+
+        var aggregatedTime = [];
+        var lastDoc = {}
+        var totalTime = 0
+        _.each(docs, (doc) => {
+
+          if (!doc.start)
+            return lastDoc = doc
+          
+          const computedTime = moment(lastDoc.time).diff(doc.time)
+          aggregatedTime.push(
+            {
+              time: computedTime,
+              human: humanizeDuration(computedTime)
+            }
+          )
+
+          totalTime+= computedTime
+
+        })
+
+        console.log(aggregatedTime)
+
+        console.log(humanizeDuration((aggregatedTime[0].time +  aggregatedTime[1].time)))
+        console.log(humanizeDuration(totalTime))
+      })
+}
+
+const buildMenu = (isStart) => {
+
+  menuItems[0].label = (isStart) ? 'Stop' : 'Start'
+  contextMenu = Menu.buildFromTemplate(menuItems)
+  tray.setContextMenu(contextMenu)
+}
+
 const createTray = () => {
   tray = new Tray(path.join(assetsDirectory, 'lazer.png'))
   // tray.setTitle('--')
 
-  // const contextMenu = Menu.buildFromTemplate([
-  //   {label: 'Quit', type: 'normal', click: () => {
-  //     app.quit()
-  //   }}
-  // ])
-
-  // tray.setContextMenu(contextMenu)
   tray.on('click', (event) => {
-    console.log('tray click')
+    
+  })
+}
 
-    db.sessions
-      .find({})
-      .sort({time: -1})
-      .limit(1)
-      .exec((err, lastDoc) => {
+const tickClick = () => {
+  console.log('tray click')
 
-      const isStart = (!lastDoc.length || lastDoc[0].start === false) ? true : false
-      const doc = {
-        time: new Date(),
-        start: isStart
-      }
+  db.sessions
+    .find({})
+    .sort({start: -1})
+    .limit(1)
+    .exec((err, lastDoc) => {
 
-      db.sessions.insert(doc, (err, newDoc) => {
-        console.log('newDoc', newDoc)
+    const isStart = (!lastDoc.length || lastDoc[0].end) ? true : false
+    
+    buildMenu(isStart)
 
-        if (newDoc.start)
-          return startClock(newDoc)
+    // _.each(contextMenu.items, (_m) => {
+    //   console.log('_m', _m.checked)
+    // })
+    // const activeMenu = _.find(contextMenu.items, {checked: true })
 
-        stopClock()
-          
+    var doc = {
+      name: 'Tezos'
+    }
+    console.log('isStart', isStart)
+    if (isStart) {
+      doc.start = new Date()
+    } else {
+      return db.sessions.update({ _id: lastDoc[0]._id}, {end: new Date()}, {}, (err, numberReplaced) => {
+        console.log(err)
+        console.log('numberReplaced', numberReplaced)
+        stopClock()      
       })
+    }
 
+    db.sessions.insert(doc, (err, newDoc) => {
+      console.log('newDoc', newDoc)
+
+      if (newDoc.start)
+        return startClock(newDoc)
+
+      stopClock()
+        
     })
+
   })
 }
 
@@ -84,12 +167,19 @@ const checkClockStatus = () => {
 
   db.sessions
     .find({})
-    .sort({time: -1})
+    .sort({start: -1})
     .limit(1)
     .exec((err, lastDoc) => {
 
-      if (!lastDoc.length || lastDoc[0].start === false)
-        return
+      if (!lastDoc.length || lastDoc[0].end)
+        return buildMenu()
+      
+      buildMenu(true)
+
+      // console.log(contextMenu.items)
+      // console.log(lastDoc[0].label)
+      var activeMenu = _.find(contextMenu.items, { label: lastDoc[0].name })
+      console.log(activeMenu.checked = true)
       
       startClock(lastDoc[0])
   })
@@ -101,7 +191,7 @@ const startClock = (newDoc) => {
   const clockCore = () => {
     console.log('t0ck', newDoc)
 
-    const startTime = moment(newDoc.time)
+    const startTime = moment(newDoc.start)
     const now = moment()
 
     const diff = now.diff(startTime, 'ms')
